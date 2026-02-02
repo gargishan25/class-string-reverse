5e91867741dca57f40e572066dfbc506c155a59b
fullycharged25 <gargishan2010@gmail.com>
2026-02-01 21:14:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-word = input("Enter a word. ")
-list_word = []
-for i in range((len(word)-1),-1,-1):
-    list_word.append(word[i])
-word = "".join(list_word)
-print(word)
+class reverse:
+    def __init__(self, s):
+        self.s = s
+    def str_reverse(self):
+        list_word = []
+        for i in range((len(self.s)-1),-1,-1):
+            list_word.append(self.s[i])
+        self.s = "".join(list_word)
+        return self.s
+s_userinp = input("Enter a word. ")
+a = reverse(s_userinp)
+print(a.str_reverse())
